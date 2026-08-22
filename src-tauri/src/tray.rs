f6bcef4839d78b4bc65063ff88da1dc45cb90ff4
root@LocalGhost <rootlocalghost@gmail.com>
2026-08-22 09:01:31 +0600

    feat(tray): implement system tray menu and icon functionality

---
+++ src-tauri/src/tray.rs
@@ -0,0 +1,65 @@
+use tauri::{
+    menu::{Menu, MenuItem, PredefinedMenuItem},
+    tray::{MouseButton, MouseButtonState, TrayIconBuilder, TrayIconEvent},
+    AppHandle, Manager,
+};
+
+pub fn create_tray(app: &AppHandle) -> Result<(), Box<dyn std::error::Error>> {
+    let show_i = MenuItem::with_id(app, "show", "Show Messenger", true, None::<&str>)?;
+    let reload_i = MenuItem::with_id(app, "reload", "Reload", true, None::<&str>)?;
+    let separator = PredefinedMenuItem::separator(app)?;
+    let quit_i = MenuItem::with_id(app, "quit", "Quit Messenger", true, None::<&str>)?;
+
+    let menu = Menu::with_items(app, &[&show_i, &reload_i, &separator, &quit_i])?;
+
+    let _tray = TrayIconBuilder::with_id("main-tray")
+        .tooltip("Messenger Desktop")
+        .icon(app.default_window_icon().unwrap().clone())
+        .menu(&menu)
+        .show_menu_on_left_click(false)
+        .on_menu_event(move |app_handle, event| match event.id.as_ref() {
+            "show" => {
+                if let Some(window) = app_handle.get_webview_window("main") {
+                    let _ = window.show();
+                    let _ = window.unminimize();
+                    let _ = window.set_focus();
+                }
+            }
+            "reload" => {
+                if let Some(window) = app_handle.get_webview_window("main") {
+                    let _ = window.eval("window.location.reload();");
+                }
+            }
+            "quit" => {
+                app_handle.exit(0);
+            }
+            _ => {}
+        })
+        .on_tray_icon_event(|tray, event| {
+            if let TrayIconEvent::Click {
+                button: MouseButton::Left,
+                button_state: MouseButtonState::Up,
+                ..
+            } = event
+            {
+                let app = tray.app_handle();
+                if let Some(window) = app.get_webview_window("main") {
+                    if let Ok(is_visible) = window.is_visible() {
+                        if is_visible {
+                            let _ = window.hide();
+                        } else {
+                            let _ = window.show();
+                            let _ = window.unminimize();
+                            let _ = window.set_focus();
+                        }
+                    } else {
+                        let _ = window.show();
+                        let _ = window.set_focus();
+                    }
+                }
+            }
+        })
+        .build(app)?;
+
+    Ok(())
+}
